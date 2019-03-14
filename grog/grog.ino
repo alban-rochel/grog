993ff15f4187d30c6a171c62be979a8dae641a1f
Alban Rochel <albanrochel@hotmail.com>
2019-03-14 15:43:05 +0100

    Ported to gamebuino. Looks like my rasterization is slightly broken.

--- grog/grog.ino
+++ grog/grog.ino
@@ -1,95 +1,12 @@
 #include <Gamebuino-Meta.h>
 #include "Engine.h"
 #include "Colors.h"
+#include "asset_car.h"
 
 #include "trigo.h"
 
 grog::Engine engine;
 
-const int32_t vertices[] {
-  (-1) << 10,  (-1) << 10, (-1) << 10,
-  (-1) << 10,  (-1) << 10, ( 1) << 10,
-  (-1) << 10,  ( 1) << 10, (-1) << 10,
-  (-1) << 10,  ( 1) << 10, ( 1) << 10,
-  ( 1) << 10,  (-1) << 10, (-1) << 10,
-  ( 1) << 10,  (-1) << 10, ( 1) << 10,
-  ( 1) << 10,  ( 1) << 10, (-1) << 10,
-  ( 1) << 10,  ( 1) << 10, ( 1) << 10
-};
-static const uint32_t faces[] {
-  0, 6, 4,
-  0, 2, 6,
-  0, 3, 2,
-  0, 1, 3,
-  2, 7, 6,
-  2, 3, 7,
-  4, 6, 7,
-  4, 7, 5,
-  0, 4, 5,
-  0, 5, 1,
-  1, 5, 7,
-  1, 7, 3
-};
-
-static const uint32_t inverted_faces[] {
-  0, 4, 6,
-  0, 6, 2,
-  0, 2, 3,
-  0, 3, 1,
-  2, 6, 7,
-  2, 7, 3,
-  4, 7, 6,
-  4, 5, 7,
-  0, 5, 4,
-  0, 1, 5,
-  1, 7, 5,
-  1, 3, 7
-};
-
-static const uint8_t colors[] {
-  grog::color(grog::Color::White), grog::color(grog::Color::White),
-  grog::color(grog::Color::Gray), grog::color(grog::Color::Gray),
-  grog::color(grog::Color::DarkGray), grog::color(grog::Color::DarkGray),
-  grog::color(grog::Color::White), grog::color(grog::Color::White),
-  grog::color(grog::Color::Gray), grog::color(grog::Color::Gray),
-  grog::color(grog::Color::DarkGray), grog::color(grog::Color::DarkGray)
-};
-
-static const uint8_t colorsX[] {
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red),
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red),
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red),
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red),
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red),
-  grog::color(grog::Color::Red), grog::color(grog::Color::Red)
-};
-
-static const uint8_t colorsY[] {
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green),
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green),
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green),
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green),
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green),
-  grog::color(grog::Color::Green), grog::color(grog::Color::Green)
-};
-
-static const uint8_t colorsZ[] {
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue),
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue),
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue),
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue),
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue),
-  grog::color(grog::Color::Blue), grog::color(grog::Color::Blue)
-};
-
-static const uint8_t outer_colors[] {
-  grog::color(grog::Color::White), grog::color(grog::Color::White),
-  grog::color(grog::Color::Gray), grog::color(grog::Color::Gray),
-  grog::color(grog::Color::DarkGray), grog::color(grog::Color::DarkGray),
-  grog::color(grog::Color::White), grog::color(grog::Color::White),
-  grog::color(grog::Color::Gray), grog::color(grog::Color::Gray),
-  grog::color(grog::Color::DarkGray), grog::color(grog::Color::DarkGray)
-};
 
 void setup()
 {
@@ -106,46 +23,45 @@ void loop()
 {
   grog::SceneNode scene;
 
-  scene.mesh.vertexBuffer = vertices;
-  scene.mesh.vertexCount = 8;
-  scene.mesh.faces = faces;
-  scene.mesh.faceCount = 12;
-  scene.mesh.colors = colors;
+  scene.mesh.vertexBuffer = grog::car_vertices;
+  scene.mesh.vertexCount = grog::car_vertexCount;
+  scene.mesh.faces = grog::car_faces;
+  scene.mesh.faceCount = grog::car_faceCount;
+  scene.mesh.colors = grog::car_colors;
 
   scene.children = new grog::SceneNode[4];
   scene.childCount = 4;
 
-  scene.children[0].mesh.vertexBuffer = vertices;
-  scene.children[0].mesh.vertexCount = 8;
-  scene.children[0].mesh.faces = faces;
-  scene.children[0].mesh.faceCount = 12;
-  scene.children[0].mesh.colors = colorsX;
+  scene.children[0].mesh.vertexBuffer = grog::wheel_vertices;
+  scene.children[0].mesh.vertexCount = grog::wheel_vertexCount;
+  scene.children[0].mesh.faces = grog::wheel_faces;
+  scene.children[0].mesh.faceCount = grog::wheel_faceCount;
+  scene.children[0].mesh.colors = grog::wheel_colors;
 
-  scene.children[1].mesh.vertexBuffer = vertices;
-  scene.children[1].mesh.vertexCount = 8;
-  scene.children[1].mesh.faces = faces;
-  scene.children[1].mesh.faceCount = 12;
-  scene.children[1].mesh.colors = colorsY;
+  scene.children[1].mesh.vertexBuffer = grog::wheel_vertices;
+  scene.children[1].mesh.vertexCount = grog::wheel_vertexCount;
+  scene.children[1].mesh.faces = grog::wheel_faces;
+  scene.children[1].mesh.faceCount = grog::wheel_faceCount;
+  scene.children[1].mesh.colors = grog::wheel_colors;
 
-  scene.children[2].mesh.vertexBuffer = vertices;
-  scene.children[2].mesh.vertexCount = 8;
-  scene.children[2].mesh.faces = faces;
-  scene.children[2].mesh.faceCount = 12;
-  scene.children[2].mesh.colors = colorsZ;
+  scene.children[2].mesh.vertexBuffer = grog::wheel_vertices;
+  scene.children[2].mesh.vertexCount = grog::wheel_vertexCount;
+  scene.children[2].mesh.faces = grog::wheel_faces;
+  scene.children[2].mesh.faceCount = grog::wheel_faceCount;
+  scene.children[2].mesh.colors = grog::wheel_colors;
 
-// outer cube
-  scene.children[3].mesh.vertexBuffer = vertices;
-  scene.children[3].mesh.vertexCount = 8;
-  scene.children[3].mesh.faces = inverted_faces;
-  scene.children[3].mesh.faceCount = 12;
-  scene.children[3].mesh.colors = outer_colors;
+  scene.children[3].mesh.vertexBuffer = grog::wheel_vertices;
+  scene.children[3].mesh.vertexCount = grog::wheel_vertexCount;
+  scene.children[3].mesh.faces = grog::wheel_faces;
+  scene.children[3].mesh.faceCount = grog::wheel_faceCount;
+  scene.children[3].mesh.colors = grog::wheel_colors;
 
-  scene.children[0].transform.identity().translate(2 << 10, 0, 0).scale(grog::floatToFixed(0.5f), grog::floatToFixed(0.5f), grog::floatToFixed(0.5f));
-  scene.children[1].transform.identity().translate(0, 2 << 10, 0).scale(grog::floatToFixed(0.5f), grog::floatToFixed(0.5f), grog::floatToFixed(0.5f));
-  scene.children[2].transform.identity().translate(0, 0, 2 << 10).scale(grog::floatToFixed(0.5f), grog::floatToFixed(0.5f), grog::floatToFixed(0.5f));
-  scene.children[3].transform.identity().scale(1 << 11, 1 << 11, 1 << 11);
+  scene.children[0].transform.identity().translate(-300, 50, 200);
+  scene.children[1].transform.identity().translate(-300, 50, -200);
+  scene.children[2].transform.identity().translate(600, 50, 200);
+  scene.children[3].transform.identity().translate(600, 50, -200);
 
-  engine.setProjection(grog::Matrix::Projection(2.f, 1, 10));
+  engine.setProjection(grog::Matrix::Projection(1.f, 1, 10));
 
   int ii = 0;
 
@@ -153,15 +69,19 @@ void loop()
   {
     while(!gb.update());
 
-    engine.setView(grog::TransformMatrix::View(1 << 10, 1 << 10, 1 << 10,
+    engine.setView(grog::TransformMatrix::View(grog::floatToFixed(-2.f), grog::floatToFixed(1.f), grog::floatToFixed(-0.4f),
                                                0, 0, 0,
-                                               0, 1 << 10, 0));
+                                               grog::floatToFixed(0), grog::floatToFixed(-1.f), 0));
 
-    scene.transform.identity()
+    scene.transform.identity();
           //.scale(1 << 10, 1 << 10, 1 << 10)
-          .rotateX(ii << 2)
-          .rotateY(ii << 1)/*
-                          .translate(40, 40, 40)*/;
+          /*.rotateX(ii << 2)
+          .rotateY(ii << 1);*/
+scene.transform.rotateY(ii);
+    scene.children[0].transform.identity().rotateZ(ii*50).translate(-300, 50, 200);
+    scene.children[1].transform.identity().rotateZ(ii*50).translate(-300, 50, -200);
+    scene.children[2].transform.identity().rotateZ(ii*50).translate(600, 50, 200);
+    scene.children[3].transform.identity().rotateZ(ii*50).translate(600, 50, -200);
 
     engine.projectScene(&scene);
 
